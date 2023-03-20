c8546c5f641f5a6ab8984e4715caac4a75271aad
Haashimi <Mohameddiini@outlook.com>
2023-03-20 03:29:29 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - print the lowercase alphabet in reverse
+ *
+ * followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
