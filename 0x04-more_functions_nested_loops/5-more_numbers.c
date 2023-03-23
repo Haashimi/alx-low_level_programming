1d174defd79c2b37135d19df22e2bf9cdbbe988b
Haashimi <Mohameddiini@outlook.com>
2023-03-23 12:56:26 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,16 @@
  */
 void more_numbers(void)
 {
-	int i = 0, j;
+	int i, j;
 
-	while (i < 10)
+	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-		while (j < 14)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(i + '0');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
-		i++;
+		_putchar('\n');
 	}
 }
