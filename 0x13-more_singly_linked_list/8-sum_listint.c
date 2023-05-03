0379f8c886dd299e6aabc506f4bbee6df15d0826
Mohamed Muhudin A. Hashim <70833715+Haashimi@users.noreply.github.com>
2023-05-03 05:36:39 +0300

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_list/8-sum_listint.c
+++ 0x13-more_singly_linked_list/8-sum_listint.c
@@ -2,15 +2,15 @@
 
 /**
  * sum_listint - function that returns the sum of all nodes
- * @h: pointer to head in function
+ * @head: pointer to head in function
  * Return: the sum result
  */
-int sum_listint(listint_t *h)
+int sum_listint(listint_t *head)
 {
-	listint_t *temp = h;
+	listint_t *temp = head;
 	int sum = 0;
 
-	if (h == NULL)
+	if (head == NULL)
 		return (0);
 
 	while (temp != NULL)
