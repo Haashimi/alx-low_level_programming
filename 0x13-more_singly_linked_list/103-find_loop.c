7b668f1432f8db8bdbaf61d72adeb0f52593982e
Mohamed Muhudin A. Hashim <70833715+Haashimi@users.noreply.github.com>
2023-05-03 06:01:36 +0300

    Update 103-find_loop.c

--- 0x13-more_singly_linked_list/103-find_loop.c
+++ 0x13-more_singly_linked_list/103-find_loop.c
@@ -1,37 +1,31 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - function that finds a loop and the begining of loop
- * @head: pointer to head in function
- * Return: pointer to the begining of loop
+ * find_listint_loop - find a node within a loop
+ *
+ * @head: pointer of the head node
+ *
+ * Return: address of the node within the loop || NULL if no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *tortoise;
-	listint_t *hare;
+	listint_t *tortoise = head, *shell = head;
 
-	if (head == NULL || head->next == NULL) /*If the list is null or just 1 node*/
-		return (NULL);
-
-	tortoise = hare = head; /*Start at the same point*/
-
-	while (tortoise && hare && hare->next)
-	{
-		tortoise = tortoise->next; /*1 step hop*/
-		hare = hare->next->next;   /*2 steps hop*/
-		if (tortoise == hare)
-			break;
-	}
-
-	if (tortoise != hare) /* if the while ended until a NULL*/
-		return (NULL);
-
-	tortoise = head; /*re-start tortoise*/
-
-	while (tortoise != hare)
+	while (shell && shell->next)
 	{
 		tortoise = tortoise->next;
-		hare = hare->next;
+		shell = shell->next->next;
+
+		if (tortoise == shell)
+		{
+			tortoise = head;
+			while (tortoise != shell)
+			{
+				tortoise = tortoise->next;
+				shell = shell->next;
+			}
+			return (shell);
+		}
 	}
-	return (hare);
+	return (NULL);
 }
