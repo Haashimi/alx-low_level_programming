6d1f76584bba16dcc73f2e963d3902dd78fa8d0c
Mohamed Muhudin A. Hashim <70833715+Haashimi@users.noreply.github.com>
2023-05-03 06:05:07 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_list/9-insert_nodeint.c
+++ 0x13-more_singly_linked_list/9-insert_nodeint.c
@@ -1,52 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node
- * at a given position.
- * @head: head of a list.
- * @idx: index of the list where the new node is
- * added.
- * @n: integer element.
+ * insert_nodeint_at_index - insert a node at particular pos
  *
- * Return: the address of the new node, or NULL if it
- * failed.
+ * @head: pointer of the head node
+ * @idx: list index to insert the node at
+ * @n: int to be added as element
+ *
+ * Return: node address on success || NULL on fail
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
-	listint_t *new;
-	listint_t *h;
+	unsigned int idex;
+	listint_t *nv_node, *ac_node;
 
-	h = *head;
-
-	if (idx != 0)
-	{
-		for (i = 0; i < idx - 1 && h != NULL; i++)
-		{
-			h = h->next;
-		}
-	}
-
-	if (h == NULL && idx != 0)
+	if (head == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	nv_node = malloc(sizeof(listint_t));
+	if (nv_node == NULL)
 		return (NULL);
 
-	new->n = n;
+	nv_node->n = n;
+	nv_node->next = NULL;
+
+	ac_node = *head;
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		new->next = h->next;
-		h->next = new;
+		nv_node->next = *head;
+		*head = nv_node;
+		return (nv_node);
 	}
 
-	return (new);
+	for (idex = 0; ac_node != NULL && idex < idx - 1; idex++)
+	{
+		ac_node = ac_node->next;
+	}
+
+	if (ac_node == NULL)
+	{
+		free(nv_node);
+		return (NULL);
+	}
+
+	nv_node->next = ac_node->next;
+	ac_node->next = nv_node;
+
+	return (nv_node);
 }
