f1672f23a79a0a374b9a1788e3ede63c082de367
Mohamed Muhudin A. Hashim <70833715+Haashimi@users.noreply.github.com>
2023-05-03 05:37:13 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_list/9-insert_nodeint.c
+++ 0x13-more_singly_linked_list/9-insert_nodeint.c
@@ -2,18 +2,18 @@
 
 /**
  * insert_nodeint_at_index - function that insert a node in a idx position
- * @h: pointer to h in function
+ * @head: pointer to head in function
  * @idx: index or position to be added
  * @n: node's value
  * Return: nth node data
  */
-listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int nodes;
-	listint_t *node_index = *h;
-	listint_t *new_node, *after_node;
+	listint_t *node_index = *head;
+	listint_t *new_node, *node_after;
 
-	if (h == NULL || *h == NULL)
+	if (head == NULL || *head == NULL)
 		return (NULL);
 
 	for (nodes = 0; node_index != NULL; nodes++)
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int n)
 	if (idx > (nodes + 1))
 		return (NULL);
 
-	node_index = *h;
+	node_index = *head;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -31,19 +31,19 @@ listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node->next = *h;
-		*h = new_node;
+		new_node->next = *head;
+		*head = new_node;
 		return (new_node);
 	}
 	else
 	{
-		after_node = *h;
+		node_after = *head;
 		for (nodes = 0; nodes < (idx - 1); nodes++)
 			node_index = node_index->next;
 		for (nodes = 0; nodes < idx; nodes++)
-			after_node = after_node->next;
+			node_after = node_after->next;
 		node_index->next = new_node;
-		new_node->next = after_node;
+		new_node->next = node_after;
 		return (new_node);
 	}
 }
