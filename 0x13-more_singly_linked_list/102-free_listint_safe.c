e6706d4ad0a0ba967711b39597a07fffa875c65e
Mohamed Muhudin A. Hashim <70833715+Haashimi@users.noreply.github.com>
2023-05-03 06:01:13 +0300

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_list/102-free_listint_safe.c
+++ 0x13-more_singly_linked_list/102-free_listint_safe.c
@@ -1,70 +1,30 @@
 #include "lists.h"
 
 /**
- * free_listp2 - function that frees a linked list
- * @head: head of a list.
- * Return: no return.
- */
-void free_listp2(listp_t **head)
-{
-	listp_t *temp;
-	listp_t *current;
-
-	if (head != NULL)
-	{
-		current = *head;
-		while ((temp = current) != NULL)
-		{
-			current = current->next;
-			free(temp);
-		}
-		*head = NULL;
-	}
-}
-
-/**
- * free_listint_safe - function that frees a linked list.
- * @h: head of a list.
- * Return: size of the list that was freed.
+ * free_listint_safe - free listint (free)
+ *
+ * @h: pointer of head node
+ *
+ * Return: list's size
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t node = 0;
-	listp_t *hptr, *new, *sum;
-	listint_t *current;
+	listint_t *act, *next;
+	size_t count = 0;
 
-	hptr = NULL;
-	while (*h != NULL)
+	if (*h == NULL || h == NULL)
+		return (0);
+
+	act = *h;
+	while (act != NULL)
 	{
-		new = malloc(sizeof(listp_t));
-
-		if (new == NULL)
-			exit(98);
-
-		new->p = (void *)*h;
-		new->next = hptr;
-		hptr = new;
-
-		sum = hptr;
-
-		while (sum->next != NULL)
-		{
-			sum = sum->next;
-			if (*h == sum->p)
-			{
-				*h = NULL;
-				free_listp2(&hptr);
-				return (node);
-			}
-		}
-
-		current = *h;
-		*h = (*h)->next;
-		free(current);
-		node++;
+		count++;
+		next = act->next;
+		free(act);
+		if (next >= act)
+			break;
+		act = next;
 	}
-
 	*h = NULL;
-	free_listp2(&hptr);
-	return (node);
+	return (count);
 }
