ba419fabc1b66b025aa4e98be623da8ae9445fbd
Mohamed Muhudin A. Hashim <70833715+Haashimi@users.noreply.github.com>
2023-05-03 05:51:27 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_list/5-free_listint2.c
+++ 0x13-more_singly_linked_list/5-free_listint2.c
@@ -1,26 +1,31 @@
 #include "lists.h"
 
 /**
- * free_listint2 - function that free memory of all list.
- * @head: pointer to head in function
- * Return: Always success.
- */
+ * free_listint2 - a function that frees the list and sets head to NULL
+ *
+ * @head: pointer to the first node
+ *
+ * Return: nothing
+*/
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
-	listint_t *next;
+	listint_t *current, *temp;
 
-	if (head == NULL)
-		return;
-	if (*head == NULL)
-		return;
-
-	current = *head;
-	while (current != NULL)
+	if (head != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		/*set head addr to current*/
+		current = *head;
+
+		/*iterate through the whole list*/
+		/*while setting the current node to temp*/
+		while ((temp = current) != NULL)
+		{
+			/*set next node to curretnt*/
+			current = current->next;
+			/*free temp, that is the current node*/
+			free(temp);
+		}
+
+		*head = NULL;
 	}
-	*head = NULL;
 }
